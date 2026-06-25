a2915bce5cacd0f36f897a58cd058b105499da39
unknown <ajaypalsingh82775@gmail.com>
2026-06-25 18:39:50 +0530

    Add GA4 Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import Script from "next/script";
 import { Navbar } from "@/components/Navbar";
 import { QuoteCTA } from "@/components/sections/QuoteCTA";
 import { Footer } from "@/components/Footer";
@@ -68,6 +69,18 @@ export default function RootLayout({
       className={`${inter.variable} ${playfair.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CJ8W2PXSD7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CJ8W2PXSD7');
+          `}
+        </Script>
         <JsonLd data={[getOrganizationSchema(), getWebsiteSchema()]} />
         <Navbar />
         <main className="flex-1">{children}</main>
